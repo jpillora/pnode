c4cfe6073281296b31b174b30ea41912c76455fd
Jaime Pillora <jpillora@gmail.com>
2013-08-06 10:25:33 +1000

    idea

--- example/peer/peer2.js
+++ example/peer/peer2.js
@@ -2,10 +2,24 @@
 //API Prototype
 
 var multinode = require('../../');
-var peer = multinode.peer('two');
 
-peer.https.listen(7000);
-peer.https.join([6000,8000]);
+var peer = multinode.peer({
+  id: 'two',
+  autoShare: true,
+  autoJoin: true
+});
+
+//net - handle + createConnection
+peer.net.join(7000, ['myhost.com','myotherhost.com']);
+
+//equivalent to:
+// net.createServer(peer.handle).listen(7000);
+// peer.createConnection(function(streamCallback) {
+//   streamCallback(net.connect('myhost.com', 7000));
+// });
+// peer.createConnection(function(streamCallback) {
+//   streamCallback(net.connect('myotherhost.com', 7000));
+// });
 
 peer.peer('one', function(remote) {
   remote.sum(1,2,3, function(err, result) {
