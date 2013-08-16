e5bf9232a42a71d6770e728eb67ff2cef24caf08
Jaime Pillora <jpillora@gmail.com>
2013-08-16 15:55:00 +1000

    more egs

--- example/client-remotes/run.js
+++ example/client-remotes/run.js
@@ -1,1 +1,44 @@
-run.js
+
+var multinode = require('../../');
+
+//START SERVER
+var server = multinode.server();
+
+server.bind('tcp://0.0.0.0:8000', function(){
+  console.log('bound to all interfaces on port 8000');
+});
+
+//START CLIENT 1
+var client1 = multinode.client({
+  id: 'client-1'
+});
+
+client1.expose({
+  one: function(date) {
+    console.log('client1: server says ' + date);
+  }
+});
+
+client1.bind('tcp://localhost:8000');
+
+//START CLIENT 2
+var client2 = multinode.client({
+  id: 'client-2'
+});
+
+client2.expose({
+  two: function(date) {
+    console.log('client2: server says ' + date);
+  }
+});
+
+client2.bind('tcp://localhost:8000');
+
+//CALL CLIENTS 
+server.client('client-1', function(remote) {
+  remote.one(new Date());
+});
+
+server.client('client-2', function(remote) {
+  remote.two(new Date());
+});
--- example/multiple-clients/run.js
+++ example/multiple-clients/run.js
@@ -1,1 +1,41 @@
-run.js
+
+var multinode = require('../../');
+
+//START SERVER
+var server = multinode.server();
+
+server.expose({
+  one: function(date) {
+    console.log('server: "one" says ' + date);
+  },
+  two: function(date) {
+    console.log('server: "two" says ' + date);
+  }
+});
+
+server.bind('tcp://0.0.0.0:8000', function(){
+  console.log('bound to all interfaces on port 8000');
+});
+
+//START CLIENT 1
+var client1 = multinode.client({
+  id: 'client-1'
+});
+
+client1.bind('tcp://localhost:8000');
+
+//START CLIENT 2
+var client2 = multinode.client({
+  id: 'client-2'
+});
+
+client2.bind('tcp://localhost:8000');
+
+//CALL SERVER 
+client1(function(remote) {
+  remote.one(new Date());
+});
+
+client2(function(remote) {
+  remote.two(new Date());
+});
