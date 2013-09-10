d6d62855f298f05a4cf8c3b3dc972f6db56c34c6
Jaime Pillora <jpillora@gmail.com>
2013-09-10 10:31:56 +1000

    testling...

--- test/testling/server.js
+++ test/testling/server.js
@@ -1,7 +1,9 @@
 
 var http = require('http');
 var fs = require('fs');
-var pnode = require('../');
+var port = parseInt(process.env.PORT,10) || 8000;
+
+var pnode = require('../../');
 
 var server = pnode.server({ id: 'server-1', debug: false });
 
@@ -13,9 +15,9 @@ server.expose({
 
 //a mini-static file server, may swap out for express or similar
 var httpServer = http.createServer(function(req, res) {
-  res.end("hello");
-}).listen(parseInt(process.env.PORT,10), function(){
-  console.log('server running at http://localhost:8000');
+  res.end(req.url === '/pnode.js' ? fs.readFileSync('browser/dist/pnode.js') : '');
+}).listen(port, function(){
+  console.log('server running on ' + port);
 });
 
 //bind to server object under the path '/pnode-ws'
